66c48a4a48965f752b1b8e0a0e539bd89dc4cc85
chandan <chandan.yeshwanth@gmail.com>
2016-12-10 23:46:43 +0530

    remove arg parse function

--- src/args.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Author: chandan
-# @Date:   2016-12-10 23:22:29
-# @Last Modified by:   chandan
-# @Last Modified time: 2016-12-10 23:29:10
-
-def parse_args(args):
-	arg_string = ' '.join(args)
-	return arg_string.split(',')
